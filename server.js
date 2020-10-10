8bfb13c71c29aaf314ab5c51b1d04a2c4830d515
Nick Minnella <nickminnella@gmail.com>
2020-10-09 23:35:35 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,72 +1,133 @@
-//var http = require('http');
-var express = require('express');
-var fs = require('fs');
-var url = require('url');
-var ROOT_DIR = "./";
-var MongoClient = require('mongodb').MongoClient;
+var express = require("express");
+var app = express();
+var port = 8080;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/static", express.static('./static/'));
+app.set('view engine', 'pug');
+app.set('views','./views');
 
-var mongoose = require('mongoose');
-var db = mongoose.connect("mongodb://localhost/mydb");
-var schema = require('./schema_file.js').somethingSchema;
-var ToDo = mongoose.model('ToDo', schema);
-
-
-mongoose.promise = require('bluebird');
-mongoose.promise = global.Promise;
-
-var dbURL = "mongodb://localhost/mydb";
-var current_list = "";
-var tasks;
-
-mongoose.connection.once('open', function () {    
-
-    http.createServer(function (req, res) {
-        if (req.method === "POST") {
-            var jsonData = "";
-            req.on('data', function (chunk) {
-                jsonData += chunk;
-            });
-
-            req.on('end', function () {
-                var requestObject = JSON.parse(jsonData);
-                var postMessage =  jsonData.substring(9, jsonData.length-2);
-                var reqMessage = JSON.stringify(requestObject);
-                var task = jsonData.substring(9, reqMessage.length-2);
-
-                var newItem = new ToDo({
-                    something: task
-                })
-                newItem.save(function (err, doc){
-
-                })
-                res.writeHead(200);
-                res.end(JSON.stringify({}));
-            });
-        } else if (req.method === "GET"){
-            var urlObj = url.parse(req.url, true, false);
-            var extended_path = "/index.html";
-            if (req.url != "/list") { 
-	           fs.readFile(ROOT_DIR + urlObj.pathname, function (err,data) {
-                    if (err) {
-                        res.writeHead(404);
-                        res.end(JSON.stringify(err));
-                        return;
-                    }
-                    
-                    res.writeHead(200);
-                    res.end(data);
-                });
-            } else if (req.url == "/list") { 
-                var dbdocs = "dbdocs";
-                var query = ToDo.find();
-                query.exec(function (err, docs) {                    
-                    for (var i in docs) {
-                        console.log(docs[i].something);
-                    }
-                    res.writeHead(200);
-                    res.end(JSON.stringify({docs}));
-                });
-            }
-        }
-    }).listen(8080);
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/w7-demo");
+var taskSchema = new mongoose.Schema({
+    item: {
+		type: String,
+		required: true,
+    	index: true,
+    	unique: true
+	}
 });
+var Task = mongoose.model("Task", taskSchema);
+
+var CircularJSON = require('circular-json');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+app.get('/first_template', function(req, res){
+   res.render('first_view');
+});
+
+
+app.get('/dynamic_view', function(req, res){
+   res.render('dynamic', {
+      name: "TutorialsPoint", 
+      url:"http://www.tutorialspoint.com"
+   });
+});
+
+
+
+app.get("/", (req, res) => {
+	var circularJSON = CircularJSON.stringify(req);
+	var split = circularJSON.split("dropItem=");
+	//console.log(req); 
+	if (split) {
+		var taskItem;
+		if (split[1]) { 
+			console.log(split[1].substring(0, split[1].indexOf('"')));
+			taskItem = split[1].substring(0, split[1].indexOf('"'));
+			MongoClient.connect(url, function(err, db) {
+  				if (err) throw err;
+  				var dbo = db.db("w7-demo");
+  				var myquery = { item:  taskItem};
+ 	 			dbo.collection("tasks").deleteOne(myquery, function(err, obj) {
+    				if (err) throw err;
+					renderList(req, res);
+    				console.log("1 item deleted: "+ taskItem);
+    				db.close();
+  				});
+			});
+			return;
+		} 
+	}
+	var query = Task.find()
+	var list = "";
+    query.exec(function (err, docs) {
+		var results = JSON.stringify(docs).split("item"); 
+		var tasks = [];
+		for (var i = 1; i < results.length; i++) {
+			var cut = results[i].substring(results.indexOf(':')+4,results[i].length);
+            var task = cut.substring(0,cut.indexOf('"'));
+			tasks.push(task);
+			list += "<li>"+task+"</li>";
+			console.log("Task "+i+": "+task);
+		}
+		console.log(tasks);
+		var complete = tasks;
+		res.render("index.ejs", { complete: complete });
+	})
+});
+
+app.delete("/", function (request, response) {
+	console.log(request.query)
+	Task.deleteOne({_id: request.query.id}).exec()
+});
+
+app.post("/addtask", (req, res) => {
+	itemName = JSON.stringify(req.body);
+    console.log(itemName);
+    var myData = new Task(req.body);
+    console.log(req.body);
+    myData.save()
+        .then(item => {
+			renderList(req, res);
+            //res.send("Todo Item saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+});
+
+app.get("/addtask", (req, res) => {
+	console.log("GET /addtask called()");
+	//console.log(req);
+	renderList(req,res);
+});
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
+
+function dropTask(taskName) {
+  console.log(document.getElementById(taskName).innerHTML);
+}
+
+function renderList(req, res) {
+	var query = Task.find()
+    var list = "";
+    query.exec(function (err, docs) {
+        var results = JSON.stringify(docs).split("item");
+        var tasks = [];
+        for (var i = 1; i < results.length; i++) {
+            var cut = results[i].substring(results.indexOf(':')+4,results[i].length);
+            var task = cut.substring(0,cut.indexOf('"'));
+            tasks.push(task);
+            list += "<li>"+task+"</li>";
+            console.log("Task "+i+": "+task);
+        }
+        console.log(tasks);
+        var complete = tasks;
+        res.render("index.ejs", { complete: complete });
+    })
+}
